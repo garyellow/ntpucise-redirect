d1b4f097cb93939ae490d2f91aaf3cb41378e875
garyellow <gary20011110@gmail.com>
2024-06-17 00:24:51 +0800

    chore: Add Google AdSense module and configuration

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -9,11 +9,21 @@ export default defineNuxtConfig({
     "~/plugins/analytics.client.ts",
   ],
   devtools: { enabled: true },
-  modules: ["@element-plus/nuxt", "@nuxtjs/seo", "@nuxtjs/tailwindcss"],
+  modules: [
+    "@element-plus/nuxt",
+    "@nuxtjs/seo",
+    "@nuxtjs/tailwindcss",
+    "@nuxtjs/google-adsense"
+  ],
   site: {
     url: process.env.NUXT_PUBLIC_BASE_URL,
     name: "國立臺北大學資訊工程學系",
     description: "這是一個臨時的跳轉網站，用於重定向到國立臺北大學資訊工程學系的真正官網。請稍等，我們會自動將您帶到最新的系所資訊、課程介紹、師資陣容及研究成果。",
     defaultLocale: "zh-TW",
+  },
+  googleAdsense: {
+    id: "ca-pub-4345340552891469",
+    overlayBottom: true,
+    pageLevelAds: true,
   }
 })
--- pages/index.vue
+++ pages/index.vue
@@ -1,18 +1,43 @@
 <template>
-  <ElSpace
-    direction="vertical"
-    class="!flex h-dvh flex-col items-center justify-center"
-  >
-    <ElButton
-      type="primary"
-      tag="a"
-      href="https://www.csie.ntpu.edu.tw/index.php"
-      rel="noopener noreferrer"
-    >
-      北大資工
-    </ElButton>
-    <ElText>點擊上面的按鈕繼續</ElText>
-  </ElSpace>
+  <div class="flex h-dvh w-full">
+    <div class="flex w-1/3 flex-col justify-between">
+      <div
+        v-for="i in 3"
+        :key="i"
+        class="h-1/3"
+      >
+        <Adsbygoogle />
+      </div>
+    </div>
+    <div class="flex w-1/3 flex-col justify-between">
+      <div class="h-1/3"><Adsbygoogle /></div>
+      <ElSpace
+        direction="vertical"
+        justify="center"
+        class="h-1/3 justify-center"
+      >
+        <ElButton
+          type="primary"
+          tag="a"
+          href="https://www.csie.ntpu.edu.tw/index.php"
+          rel="noopener noreferrer"
+        >
+          北大資工
+        </ElButton>
+        <ElText>點擊上面的按鈕繼續</ElText>
+      </ElSpace>
+      <div class="h-1/3"><Adsbygoogle /></div>
+    </div>
+    <div class="flex w-1/3 flex-col justify-between">
+      <div
+        v-for="i in 3"
+        :key="i"
+        class="h-1/3"
+      >
+        <Adsbygoogle />
+      </div>
+    </div>
+  </div>
 </template>
 
 <script lang="ts" setup></script>
