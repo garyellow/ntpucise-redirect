03f9ffcd63d863b7cbebc82bcc6734d2dd2707f0
garyellow <gary20011110@gmail.com>
2024-06-17 01:04:08 +0800

    chore: Update Google AdSense configuration and add ads to index.vue

--- pages/index.vue
+++ pages/index.vue
@@ -1,16 +1,44 @@
 <template>
   <div class="flex h-dvh w-full">
     <div class="flex w-1/3 flex-col justify-between">
-      <div
-        v-for="i in 3"
-        :key="i"
-        class="h-1/3"
-      >
-        <Adsbygoogle />
+      <div class="h-1/3">
+        <Adsbygoogle
+          ad-client="ca-pub-4345340552891469"
+          ad-slot="9235145893"
+          ad-format="auto"
+          :ad-full-width-responsive="true"
+          :ad-style="{ display: 'block' }"
+        />
+      </div>
+      <div class="h-1/3">
+        <Adsbygoogle
+          ad-client="ca-pub-4345340552891469"
+          ad-slot="7538920847"
+          ad-format="auto"
+          :ad-full-width-responsive="true"
+          :ad-style="{ display: 'block' }"
+        />
+      </div>
+      <div class="h-1/3">
+        <Adsbygoogle
+          ad-client="ca-pub-4345340552891469"
+          ad-slot="4149506029"
+          ad-format="auto"
+          :ad-full-width-responsive="true"
+          :ad-style="{ display: 'block' }"
+        />
       </div>
     </div>
     <div class="flex w-1/3 flex-col justify-between">
-      <div class="h-1/3"><Adsbygoogle /></div>
+      <div class="h-1/3">
+        <Adsbygoogle
+          ad-client="ca-pub-4345340552891469"
+          ad-slot="3599675837"
+          ad-format="auto"
+          :ad-full-width-responsive="true"
+          :ad-style="{ display: 'block' }"
+        />
+      </div>
       <ElSpace
         direction="vertical"
         justify="center"
@@ -22,19 +50,47 @@
           href="https://www.csie.ntpu.edu.tw/index.php"
           rel="noopener noreferrer"
         >
-          北大資工
+          北大資工官網
         </ElButton>
         <ElText>點擊上面的按鈕繼續</ElText>
       </ElSpace>
-      <div class="h-1/3"><Adsbygoogle /></div>
+      <div class="h-1/3">
+        <Adsbygoogle
+          ad-client="ca-pub-4345340552891469"
+          ad-slot="8660430826"
+          ad-format="auto"
+          :ad-full-width-responsive="true"
+          :ad-style="{ display: 'block' }"
+        />
+      </div>
     </div>
     <div class="flex w-1/3 flex-col justify-between">
-      <div
-        v-for="i in 3"
-        :key="i"
-        class="h-1/3"
-      >
-        <Adsbygoogle />
+      <div class="h-1/3">
+        <Adsbygoogle
+          ad-client="ca-pub-4345340552891469"
+          ad-slot="8221447124"
+          ad-format="auto"
+          :ad-full-width-responsive="true"
+          :ad-style="{ display: 'block' }"
+        />
+      </div>
+      <div class="h-1/3">
+        <Adsbygoogle
+          ad-client="ca-pub-4345340552891469"
+          ad-slot="6908365451"
+          ad-format="auto"
+          :ad-full-width-responsive="true"
+          :ad-style="{ display: 'block' }"
+        />
+      </div>
+      <div class="h-1/3">
+        <Adsbygoogle
+          ad-client="ca-pub-4345340552891469"
+          ad-slot="9342957107"
+          ad-format="auto"
+          :ad-full-width-responsive="true"
+          :ad-style="{ display: 'block' }"
+        />
       </div>
     </div>
   </div>
